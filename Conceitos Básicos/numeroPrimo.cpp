87500b8c3e54f92c9f19c1cbf3d8419affecb987
hzd4m <120053935+hzd4m@users.noreply.github.com>
2025-03-22 18:51:57 +0000

    Número Primo

---
+++ Conceitos Básicos/numeroPrimo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+bool ehPrimo(int n){
+    if(n < 2){
+        return false;
+    }else{
+        for(int i = 2 ; i * i ; i++){
+            
+        }
+    }
+}
